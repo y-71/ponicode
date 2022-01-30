5d0120a652c946262ad08eb4a403cab84200fccc
tawfik boujeh <tawfik@wonder.me>
2022-01-30 15:45:02 +0100

    feat: test file with yarn CLI

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("ponicode 🦄🦄🦄");
+import runTestcaseFile from "./util/runTestcaseFile";
+
+const args = process.argv;
+if (args.length !== 3) {
+  throw new Error("Wrong number of arguments provided. Exitting.");
+}
+const filePath = args[2];
+
+const solution = runTestcaseFile(filePath);
+console.log(
+  "🦄🎢🎢🎢🎢🎢🎢 The earnings will be : " + solution + " Dirhams 🤑"
+);
